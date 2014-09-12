8c0c703fa49db3f49c848b55737bcf212bfeeace
Michael Ganger <mganger747@gmail.com>
2014-09-12 08:12:24 -0400

    Fixed serial.h library

--- Arduino/lib/Serial/Serial.h
+++ Arduino/lib/Serial/Serial.h
@@ -1,7 +1,7 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-static const endl = "\r\n";
+static const char* endl = "\r\n";
 
 template<class T>
 inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; }
