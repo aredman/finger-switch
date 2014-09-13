0894e695bb11436b2b629268c5abf51cf260a225
Michael Ganger <mganger747@gmail.com>
2014-09-13 14:11:38 -0400

    Added calibration mode to the arduino.

--- Arduino/src/sketch.ino
+++ Arduino/src/sketch.ino
@@ -10,8 +10,22 @@
 //simple function to block. I'm too lazy to make a class
 bool block(){
 	if(Serial.available() == 0) return true;
+	if(Serial.read() == 'r') calibration();
 	Serial.read();
 	return false;
+}
+
+void calibration(){
+	Accel accelerometer;
+	Magnet magnetometer;
+	VectPrinter<int> printer;
+
+	while(1){
+		printer.add(accelerometer.read().getData());
+		printer.add(magnetometer.read().getData());
+		printer.print();
+		if(Serial.read() == 'r') return;
+	}
 }
 
 void setup() {
