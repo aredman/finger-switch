1bf166dfc098f9a485aa0290fbe7cdb922c544cf
Michael Ganger <mganger747@gmail.com>
2014-09-13 18:09:04 -0400

    Changed the linear test program to try out the new class

--- Parser/src/linearTest.cpp
+++ Parser/src/linearTest.cpp
@@ -21,4 +21,39 @@ int main(){
 
 	std::vector<long double> newVector = testCal.fixVector(random);
 	for(int i = 0; i < newVector.size(); i++) std::cout << newVector[i] << std::endl;
+
+	//test the calibration of a big table of values
+	//first make the vector
+	std::vector< std::vector<int> > testTable;
+	for(int i = 0; i < 10; i++){
+		std::vector<int> tmp(3);
+		for(int j = 0; j < 3; j++){
+			tmp[j] = rand()/1000000;
+		}
+		testTable.push_back(tmp);
+	}
+
+
+	for(int i = 0; i < testTable.size(); i++){
+		for(int j = 0; j < 3; j++){
+			std::cout << testTable[i][j] << ';';
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "=====================" << std::endl;
+
+	std::vector< Calibration<int> > xyzCal;
+	xyzCal.push_back(Calibration<int>(100,345));
+	xyzCal.push_back(Calibration<int>(137,302));
+	xyzCal.push_back(Calibration<int>(275,163));
+
+	SensorCalibration<int> calibrator(xyzCal);
+	std::vector< std::vector<long double> > testTableCal = calibrator.fixTable(testTable);
+	for(int i = 0; i < testTableCal.size(); i++){
+		for(int j = 0; j < testTableCal[i].size(); j++){
+			std::cout << testTableCal[i][j] << ';';
+		}
+		std::cout << std::endl;
+	}
 }
