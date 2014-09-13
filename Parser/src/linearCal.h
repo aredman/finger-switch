431200eb26b8f97834be5a204208f969d91efaa5
Michael Ganger <mganger747@gmail.com>
2014-09-13 18:08:39 -0400

    Added class to the calibrations that does a whole array at once

--- Parser/src/linearCal.h
+++ Parser/src/linearCal.h
@@ -25,3 +25,41 @@ class Calibration {
 			return output;
 		}
 };
+
+
+template <class Type>
+class SensorCalibration {
+	public:
+		std::vector< Calibration<int> > xyzCal;
+
+		SensorCalibration(std::vector< Calibration<int> > inputCals){
+			xyzCal = inputCals;
+		}
+
+		std::vector< std::vector<long double> > fixTable(std::vector< std::vector<Type> > input){
+			//make new vector for xyz
+			std::vector<Type> xvector, yvector, zvector;
+			for(int i = 0; i < input.size(); i++){
+				xvector.push_back(input[i][0]);
+				yvector.push_back(input[i][1]);
+				zvector.push_back(input[i][2]);
+			}
+
+			//calibrate the vectors individually
+			std::vector<long double> xvectorCal, yvectorCal, zvectorCal;
+			xvectorCal = xyzCal[0].fixVector(xvector);
+			yvectorCal = xyzCal[1].fixVector(yvector);
+			zvectorCal = xyzCal[2].fixVector(zvector);
+
+			//put them back into a calibrated vector and return it
+			std::vector< std::vector<long double> > output;
+			for(int i = 0; i < input.size(); i++){
+				std::vector<long double> tmp;
+				tmp.push_back(xvectorCal[i]);
+				tmp.push_back(yvectorCal[i]);
+				tmp.push_back(zvectorCal[i]);
+				output.push_back(tmp);
+			}
+			return output;
+		}
+};
