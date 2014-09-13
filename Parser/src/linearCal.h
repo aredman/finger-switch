dd4106f754be6a88b9f593471cc9c094600f270f
Michael Ganger <mganger747@gmail.com>
2014-09-13 17:01:58 -0400

    Added new functions to the library to take a vector and calibrate it, returning a long double vector

--- Parser/src/linearCal.h
+++ Parser/src/linearCal.h
@@ -1,16 +1,27 @@
 //library to facilitate linear calibration curves
 
+#include <vector>
+#include <iostream>
+
 template <class Type>
 class Calibration {
-	long double m;
-	long double b;
+	public:
+		long double m;
+		long double b;
 
-	Calibration(long double mInput, long double bInput){
-		m = mInput;
-		b = bInput;
-	}
+		Calibration(long double mInput, long double bInput){
+			m = mInput;
+			b = bInput;
+		}
 
-	long double fix(Type input){
-		return (input - b) / m;
-	}
+		long double fix(Type input){
+			return (input - b) / m;
+		}
+
+		std::vector<long double> fixVector(std::vector<Type> input){
+			int size = input.size();
+			std::vector<long double> output;
+			for(int i = 0; i < size; i++) output.push_back(fix(input[i]));
+			return output;
+		}
 };
