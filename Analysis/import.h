9fb0bb5ca6e1060734e97f7b0b10c77d66a33079
aredman <aredman314@gmail.com>
2014-09-13 15:46:07 -0400

    Finished import library to turn string streams to vectors of vectors of integers

--- Analysis/import.h
+++ Analysis/import.h
@@ -1,17 +1,17 @@
 #ifndef IMPORT_H
 #define IMPORT_H
 
-#include <fstream>;
-#include <stdlib.h>;
-#include <sstream>;
-#include <iostream>;
-#include <vector>;
+#include <fstream>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
+#include <vector>
 
 class Import{	
 	public:
-		std::vector<std::string> addline(istream& inputFile){
+		std::vector<std::string> addline(std::istream& inputFile){
 			std::vector<std::string> inputVector;
-			while(inputFile.eof == 0){
+			while(inputFile.eof() == 0){
 				std::string line;
 				std::getline(inputFile,line);	
 				inputVector.push_back(line);
@@ -20,20 +20,20 @@ class Import{
 		}
 
 		std::vector<int> parseLine(std::string inputLine){
-			std::vector<std::int> numberData;
-			stringstream inputStream(inputLine);
+			std::vector<int> numberData;
+			std::stringstream inputStream(inputLine);
 			std::string piece;
-			while(inputStream.eof==0){
+			while(inputStream.eof()==0){
 				std::getline(inputStream,piece,';');
 				numberData.push_back(atoi(piece.c_str()));
 			}
 			return numberData;
 		}
 
-		std::vector<std::vector<int>> returnVector(std::vector<std::string> inputLine){
-			std::vector<std::vector<int>> final;
+		std::vector< std::vector<int> > returnVector(std::vector<std::string> inputLine){
+			std::vector< std::vector<int> > final;
 			for(int i = 0;i < inputLine.size();i++){
-				final.push_back = parseLine(inputLine[i]);
+				final.push_back(parseLine(inputLine[i]));
 			}
 			return final;
 		}
