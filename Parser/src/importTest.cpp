cddb4385aca0a680ca3a39eaa847670a61616e59
aredman <aredman314@gmail.com>
2014-09-13 18:10:00 -0400

    Test space for working on import and it's related functions. Currently shows that cut works.

--- Parser/src/importTest.cpp
+++ Parser/src/importTest.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 int main(){
 	intTable table;
+	intTable point0;
 	std::string filename = "test1";
 	Import data;
 	table = data.tabulate(filename);
-	for(int j = 0; j<table.size();j++){
-		for(int i = 0;i<table[i].size();i++){
-			std::cout << table[j][i];
+	cout << "hello World" << endl;
+	point0 = data.cut(table,4);
+	cout << "hello World" << endl;
+
+
+	for(int i = 0; i<point0.size();i++){
+		for(int j = 0;j<point0[i].size();j++){
+			std::cout << point0[i][j];
 			std::cout << ",";
 		}
 		std::cout << std::endl;
