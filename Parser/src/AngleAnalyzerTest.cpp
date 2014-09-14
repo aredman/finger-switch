2c157799cd54b49c1a248a28e1e2061312f6d2ed
Michael Ganger <mganger747@gmail.com>
2014-09-14 19:54:33 -0400

    started working on the test file

--- Parser/src/AngleAnalyzerTest.cpp
+++ Parser/src/AngleAnalyzerTest.cpp
@@ -1,1 +1,16 @@
 //this program tests the angle analyzer
+
+#include <AngleAnalyzer.h>
+#include <iostream>
+
+typedef long double ldouble
+
+using namespace std;
+
+int main(){
+	//test the measurement class
+	Measurement<ldouble> testM(234, 23);
+
+	cout << "value " << testM.getValue() << endl;
+	cout << "uncertainty " << testM.getInaccuracy() << endl;
+}
