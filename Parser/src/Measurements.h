bd90a0a0afa01dd33fcf2a9073327260302f2cc7
aredman <aredman314@gmail.com>
2014-09-14 21:46:12 -0400

    Added comments explaining import.h and Measurements.h

--- Parser/src/Measurements.h
+++ Parser/src/Measurements.h
@@ -5,6 +5,12 @@
 
 typedef std::vector< std::vector<int> > intTable;
 
+//A class made to store the data measurements in lists of tables for easier
+//handeling. The functions are simple set and get methods. The objects are
+//vectors containing intTables. Which are defined above as two vectors 
+//containing integers
+
+
 class RawMeasurementTables{
 	private:
 		std::vector<intTable> accelerometerData;
--- Parser/src/import.h
+++ Parser/src/import.h
@@ -9,7 +9,13 @@
 #include "Measurements.h"
 
 class Import{	
+
+	private:
+
 	public:
+
+		//Takes an input stream from a file and saves it as a vector of 
+		//strings.
 		std::vector<std::string> addline(std::istream& inputFile){
 			std::vector<std::string> inputVector;
 			while(inputFile.eof() == 0){
@@ -21,6 +27,9 @@ class Import{
 			return inputVector;
 		}
 
+		//Takes a string and converts it into a vector of integers
+		//Parses the data using getline based on semi-colons and 
+		//converts them from ascii to integers using atoi()
 		std::vector<int> parseLine(std::string inputLine){
 			std::vector<int> numberData;
 			std::stringstream inputStream(inputLine);
@@ -32,6 +41,7 @@ class Import{
 			return numberData;
 		}
 
+		//Takes a string of data and separates it into an intTable
 		intTable returnTable(std::vector<std::string> inputLine){
 			intTable final;
 			for(int i = 0;i < inputLine.size();i++){
@@ -40,6 +50,8 @@ class Import{
 			return final;
 		}
 
+		//Takes a file name and opens a filestream to that data file
+		//Parses the semi-colon separated data into a intTable
 		intTable tabulate(std::string filename){
 			std::ifstream inputStream;
 			inputStream.open(filename.c_str());
@@ -51,6 +63,7 @@ class Import{
 			return dataArray;
 		}
 
+		//Cuts a big table into smaller tables based on row index numbers
 		intTable rowCut(intTable bigTable,int index){
 			intTable smallTable;
 			for(int i = 0;i<bigTable.size();i++){
@@ -61,6 +74,7 @@ class Import{
 			return smallTable;
 		}
 
+		//Cuts a big table into smaller tables based on columns
 		intTable columnCut(intTable bigTable,int abound, int bbound){
 			intTable smallTable;
 			for(int i = 0; i < bigTable.size();i++){
@@ -74,6 +88,7 @@ class Import{
 			return smallTable;
 		}
 
+		//Cuts a table up based on row index and column boundaries
 		intTable tableCut(intTable bigTable,int index,int abound,int bbound){
 				intTable mediumTable;
 				intTable smallTable;
@@ -83,6 +98,7 @@ class Import{
 				return smallTable;
 		}
 
+		//Pulls accelerometer data out of an intTable
 		std::vector<intTable> getAccel(intTable bigTable,int rows){
 			std::vector<intTable> accelerometerData;
 			for(int i = 0; i<rows;i++){
@@ -91,6 +107,7 @@ class Import{
 			return accelerometerData;
 		}
 
+		//Pulls magnetometer data out of an intTable 
 		std::vector<intTable> getMagnet(intTable bigTable,int rows){
 			std::vector<intTable> magnetometerData;
 			for(int i = 0; i<rows;i++){
@@ -98,6 +115,9 @@ class Import{
 			}
 			return magnetometerData;
 		}
+
+		//"explodes" an intTable into multiple smaller intTables 
+		//and returns a vector of smaller intTables
 		std::vector<intTable> explode(intTable bigTable,int rows){
 			std::vector<intTable> array;
 			for(int i = 0; i<rows;i++){
