56dd2f0551e9324a8d05b8f2ee9943bf9aacda3e
Michael Ganger <mganger747@gmail.com>
2014-09-15 13:45:41 -0400

    Started working on the main program

--- Parser/src/main.cpp
+++ Parser/src/main.cpp
@@ -1,6 +1,14 @@
 //this is the main program to parse the data.
 //As arguments, it takes the data file, and two calibration files for the sensors
 
-#incl
+#include "AngleAnalyzer.h"
+#include "import.h"
+#include "linearCal.h"
+#include "Measurements.h"
+#include "Stats.h"
 
-int main()
+#include <vector>
+
+int main(int argc, char** argv){
+	
+}
