232996817cf58f6570bc7201e9716ecae0475ce8
Michael Ganger <mganger747@gmail.com>
2014-09-15 15:55:55 -0400

    imported the data into the program

--- Parser/src/main.cpp
+++ Parser/src/main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int main(int argc, char** argv){
 	//get the aruments as a string object
-	if(argc != 4){
+	if(argc != 3){
 		cout << "Wrong numnber of arguments supplied" << endl;
 		exit(1);
 	}
@@ -25,15 +25,18 @@ int main(int argc, char** argv){
 	//print information
 	cout << "Extracting data from:       " << args.get()[1] << endl;
 	cout << "Accelerometer Calibrations: " << args.get()[2] << endl;
-	cout << "Magnetometer Calibrations:  " << args.get()[3] << endl;
 
 
 	//with this information, import the data file
-	Import importedData(args.get()[1]);
+	Import importedData;
+	importedData.tabulate(args.get()[1]);
 	
 
 	//after getting a vector of the data (of accelerometer data) calibrate the vectors
 	//using the calibration files (requires another library to load the vectors)
+	Import importCal;
+	vector< vector<int> > importedCalVector = importCal.tabulate(args.get()[2]);
+
 	vector< Calibration< Measurement<int> > > accelCalVector;
 	vector< Calibration< Measurement<int> > > magnetCalVector;
 
@@ -41,38 +44,38 @@ int main(int argc, char** argv){
 	//==========================================================================================
 	//with the calibrated data, calculate the angle measurements, taking care to propagate error
 	//vector of calibrations
-	vector< Measurement<int> > accelCals;
-	vector< Measurement<int> > magnetCals;
+	//vector< Measurement<int> > accelCals;
+	//vector< Measurement<int> > magnetCals;
 
 	//calibrate the magnetometer data
-	SensorCalibration accelSensorCal(calibrations);
-	SensorCalibration magnetSensorCal(calibrations);
+	//SensorCalibration accelSensorCal(calibrations);
+	//SensorCalibration magnetSensorCal(calibrations);
 
-	vector< vector< vector<double> > > accelFixed;
-	vector< vector< vector<double> > > magnetFixed;
+	//vector< vector< vector<long double> > > accelFixed;
+	//vector< vector< vector<long double> > > magnetFixed;
 
-	for(int i = 0; i < importedData.getAccel().size(); i++){
-		//fix both the acceleration and magnetometer data
-		accelSensorCal.fixTable(importedData.getAccel()[i]);
-		magnetSensorCal.fixTable(importedData.getMagnet()[i]);
-	}
+	//for(int i = 0; i < importedData.getAccel().size(); i++){
+		////fix both the acceleration and magnetometer data
+		//accelSensorCal.fixTable(importedData.getAccel()[i]);
+		//magnetSensorCal.fixTable(importedData.getMagnet()[i]);
+	//}
 	
 	//output to files representing the calibrated angles
-	for(int i = 0; i < accelFixed.size(); i++){
+	//for(int i = 0; i < accelFixed.size(); i++){
 		//accel
-		string accelName("accel");
-		accelName += to_string(i);
-		ofstream AccelFile;
-		AccelFile.open(accelName, ios::trunc);
-		printVector(AccelFile, accelName);
+		//string accelName("accel");
+		//accelName += to_string(i);
+		//ofstream AccelFile;
+		//AccelFile.open(accelName, ios::trunc);
+		//printVector(AccelFile, accelName);
 
 		//magnet
-		string magnetName("magnet");
-		magnetName += to_string(i);
-		ofstream MagnetFile;
-		MagnetAccelFile.open(magnetName, ios::trunc);
-		printVector(MagnetFile, magnetName);
-	}
+		//string magnetName("magnet");
+		//magnetName += to_string(i);
+		//ofstream MagnetFile;
+		//MagnetAccelFile.open(magnetName, ios::trunc);
+		//printVector(MagnetFile, magnetName);
+	//}
 
 	//=======================================================================
 	//with the angles, construct an array of ijk vectors, using the simple algorithm, and add the error measurements
