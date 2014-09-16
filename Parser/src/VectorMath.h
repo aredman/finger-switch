a706f5d109036102075e806af39859501990c016
Michael Ganger <mganger747@gmail.com>
2014-09-16 17:35:04 -0400

    Made a library to do vector math with arbitrary vectors (as long as they are the same size)

--- Parser/src/VectorMath.h
+++ Parser/src/VectorMath.h
@@ -1,1 +1,45 @@
 //this library is for doing math 3 dimensional vectors
+
+#include <valarray>
+#include <iostream>
+
+template <class Type>
+double dotProduct(Type v1, Type v2){
+	if(v1.size() != v2.size()){
+		std::cout << "Vectors have different sizes" << std::endl;
+		return 0;
+	}
+	double sum = 0;
+	for(int i = 0; i < v1.size(); i++){
+		sum += v1[i] * v2[i];
+	}
+	return sum;
+}
+
+template <class Type>
+Type projection(Type v1, Type v2){
+	if(v1.size() != v2.size()){
+		std::cout << "Vectors have different sizes" << std::endl;
+		return Type(3);
+	}
+	double scalar = dotProduct(v1, v2) / dotProduct(v2, v2);
+	Type returnVector(v1.size());
+	for(int i = 0; i < v1.size(); i++){
+		returnVector[i] = v2[i] * scalar;
+	}
+	return returnVector;
+}
+
+template <class Type>
+Type rejection(Type v1, Type v2){
+	if(v1.size() != v2.size()){
+		std::cout << "Vectors have different sizes" << std::endl;
+		return Type(3);
+	}
+	Type projectedVector = projection(v1, v2);
+	Type returnVector(v1.size());
+	for(int i = 0; i < v1.size(); i++){
+		returnVector[i] = v1[i] - projectedVector[i];
+	}
+	return returnVector;
+}
