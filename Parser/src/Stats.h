5acbb7bcf62a623f0e848f1cd4dd9a51833aad40
Michael Ganger <mganger747@gmail.com>
2014-09-16 20:31:23 -0400

    changed the stats library to use doubles, and added a function to take the mean of a table of doubles (by column). Also added a test to the StatsTest program

--- Parser/src/Stats.h
+++ Parser/src/Stats.h
@@ -4,32 +4,45 @@
 
 #include <vector>
 #include <math.h>
+#include <iostream>
 
-template <class Type>
 class Stats {
 	public:
 		Stats(){}
-		long double mean(std::vector<Type> input){
+		double mean(std::vector<double> input){
 			int size = input.size();
-			long double sum = 0;
+			double sum = 0;
 			for(int i = 0; i < size; i++) sum += input[i];
 			return sum / size;
 		}
-		long double variance(std::vector<Type> input){
+		std::vector<double> vectorMean(std::vector< std::vector<double> > input){
+			//create a new vector to hold the means
+			std::vector<double> output;
+			for(int i = 0; i < input[0].size(); i++){
+				//make a vector of the input size
+				std::vector<double> tmp;
+				for(int j = 0; j < input.size(); j++){
+					tmp.push_back(input[j][i]);
+				}
+				output.push_back(mean(tmp));
+			}
+			return output;
+		}
+		double variance(std::vector<double> input){
 			//variance is the sum of the squares, divided by the DOF, subtracting the mean
 			int size = input.size();
-			long double average = mean(input);
-			long double sum = 0;
+			double average = mean(input);
+			double sum = 0;
 			for(int i = 0; i < size; i++){
-				long double difference = input[i] - average;
+				double difference = input[i] - average;
 				sum += difference * difference;
 			}
 			return sum / size;
 		}
-		long double stdDev(std::vector<Type> input){
+		double stdDev(std::vector<double> input){
 			return sqrt(variance(input));
 		}
-		long double accuracy(std::vector<Type> input){
+		double accuracy(std::vector<double> input){
 			return stdDev(input) / sqrt(input.size());
 		}
 };
--- Parser/src/StatsTest.cpp
+++ Parser/src/StatsTest.cpp
@@ -6,17 +6,35 @@
 #include <vector>
 #include <iostream>
 
+using namespace std;
+
 int main(){
 	//make a vector of random numbers
 	srand (time(NULL));
-	std::vector<int> random;
-	for(int i = 0; i < 100000000; i++){
+	std::vector<double> random;
+	for(int i = 0; i < 100; i++){
 		random.push_back(rand()/1000000);
 	}
 
 	//perform statistical analysis
-	std::cout << "Mean:     " << Stats<int>().mean(random) << std::endl;
-	std::cout << "Variance: " << Stats<int>().variance(random) << std::endl;
-	std::cout << "StdDev:   " << Stats<int>().stdDev(random) << std::endl;
-	std::cout << "Accuracy: " << Stats<int>().accuracy(random) << std::endl;
+	std::cout << "Mean:     " << Stats().mean(random) << std::endl;
+	std::cout << "Variance: " << Stats().variance(random) << std::endl;
+	std::cout << "StdDev:   " << Stats().stdDev(random) << std::endl;
+	std::cout << "Accuracy: " << Stats().accuracy(random) << std::endl;
+
+
+	vector< vector<double> > vectorTest;
+	for(int i = 0; i < 10; i++){
+		vector<double> tmp;
+		for(int j = 0; j < 10; j++){
+			tmp.push_back(rand()/1000000);
+		}
+		vectorTest.push_back(tmp);
+	}
+
+	vector<double> vectorTestMean = Stats().vectorMean(vectorTest);
+
+	for(int i = 0; i < vectorTestMean.size(); i++) cout << vectorTestMean[i] << ';';
+	cout << endl;
+	
 }
